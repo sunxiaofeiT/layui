9ade2bf5036878c5737fbfa831d8521cf1b282ef
zhuchenlu <18222828739@163.com>
2018-06-13 14:05:15 +0800

    style添加

---
+++ src/lay/modules/button/button.js
@@ -0,0 +1,16 @@
+// add button
+    $('#add').on('click', function() {
+        alert("add");
+    })
+    // edit button
+    $('#edit').on('click', function() {
+        alert("edit");
+    })
+    // delete button
+    $('#del').on('click', function() {
+        alert("delete");
+    })
+    //view button
+    $('#view').on('click', function() {
+        alert("view");
+    })
