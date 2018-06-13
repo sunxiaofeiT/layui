bd576c65e3ab768bdba0ba706bfdf5f809482f1a
yuchunyu97 <yuchunyu97@gmail.com>
2018-06-13 15:24:06 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -179,7 +179,7 @@ var argv = require('minimist')(process.argv.slice(2), {
         mv: function (ver) {
                 ver = ver === 'open';
 
-                var src = ['./src/**/*.{png,jpg,gif,html,mp3,json}', './src/plugins/**/*'],
+                var src = ['./src/**/*.{png,jpg,gif,html,mp3,json,eot,svg,ttf,woff,woff2}'],
                     dir = destDir(ver);
 
                 if (ver || argv.open) {
